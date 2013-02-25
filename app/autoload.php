07317acf48de52820fce69197cdbf19febc3e4ec
tmagiera <tadeusz.magiera@gmail.com>
2013-02-25 10:25:12 +0100

    Dzialajace Entity

--- app/autoload.php
+++ app/autoload.php
@@ -4,6 +4,8 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 
 $loader = require __DIR__.'/../vendor/autoload.php';
 
+AnnotationRegistry::registerFile(__DIR__.'/../vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php');
+
 // intl
 if (!function_exists('intl_get_error_code')) {
     require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
