a5fe0755846dcc3bc9e1aac12e1bae1d360b6424
tmagiera <tadeusz.magiera@gmail.com>
2013-03-02 23:29:42 +0100

    Deployu na produkcje ciag dalszy

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,4 +1,3 @@
-set :shared_files,      ["app/config/parameters.yml"]
 set :shared_children,     [app_path + "/logs", web_path + "/uploads", "vendor"]
 set :use_composer, true
 set :update_vendors, true
@@ -19,5 +18,22 @@ role  :web,           domain
 role  :app,           domain
 role  :db,            domain, :primary => true
 
-set   :use_sudo,      false
+set   :use_sudo,      true
 set   :keep_releases, 3
+
+namespace :customs do
+    desc "Change user and group to www_data"
+    task :chown_to_www_data, :roles => [ :app, :db, :web ] do
+	    sudo "chown -R www-data:www-data #{deploy_to}"
+    end
+
+    desc "Symlinking parameters.yml to production parameteres"
+    task :symlink, :roles => :app do
+        run <<-CMD
+        sudo "ln -nfs #{release_path}/app/config/parameters_prod.yml #{release_path}/app/config/parameters.yml"
+        CMD
+    end
+end
+
+after "deploy:symlink","customs:symlink"
+after "deploy","customs:chown_to_www_data"
