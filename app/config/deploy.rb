8f87f2d37e52d477d350bd33464c2dd5664c857a
tmagiera <tadeusz.magiera@gmail.com>
2013-03-04 11:23:05 +0100

    Uruchomienie produkcyjne c.d.

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -4,12 +4,13 @@ set :update_vendors, true
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
-ssh_options[:auth_methods] = "publickey"
-ssh_options[:keys] = ["/home/tmagiera/Pobrane/tmagiera.pem"]
+#ssh_options[:auth_methods] = "publickey"
+#ssh_options[:keys] = ["/home/tmagiera/Pobrane/tmagiera.pem"]
 
 set   :application,   "Treneo"
 set   :deploy_to,     "/var/www/treneo"
-set   :domain,        "ubuntu@ec2-54-228-75-68.eu-west-1.compute.amazonaws.com"
+#set   :domain,        "ubuntu@ec2-54-228-75-68.eu-west-1.compute.amazonaws.com"
+set   :domain,        "www-data@192.168.91.128"
 
 set   :scm,           :git
 set   :repository,    "https://tmagiera:haslo123@github.com/tmagiera/symfony.git"
@@ -18,22 +19,17 @@ role  :web,           domain
 role  :app,           domain
 role  :db,            domain, :primary => true
 
-set   :use_sudo,      true
+set   :use_sudo,      false
 set   :keep_releases, 3
 
 namespace :customs do
-    desc "Change user and group to www_data"
-    task :chown_to_www_data, :roles => [ :app, :db, :web ] do
-	    sudo "chown -R www-data:www-data #{deploy_to}"
-    end
-
     desc "Symlinking parameters.yml to production parameteres"
     task :symlink, :roles => :app do
         run <<-CMD
-        sudo "ln -nfs #{release_path}/app/config/parameters_prod.yml #{release_path}/app/config/parameters.yml"
+        ln -nfs #{release_path}/app/config/parameters_prod.yml #{release_path}/app/config/parameters.yml
         CMD
     end
 end
 
 after "deploy:symlink","customs:symlink"
-after "deploy","customs:chown_to_www_data"
+#after "deploy","customs:chown_to_www_data"
